d670d5bce3460b5c6ed36a98b2574bd144bdd5d5
Giancarlo Ramos <giancarlo.ramos@janaq.com>
2024-05-05 12:48:49 -0500

    obtener la coordenada del click

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
 });
